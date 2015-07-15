2fb9eb192572cfcd3ddcb4fd7f8d4816d86170ed
Evan Griffiths <griffithse@gmail.com>
2015-07-15 12:51:47 -0400

    db seed file created

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 
+
 gulp.task('servertests', function() {
   process.env.NODE_ENV = 'testing';
   return gulp.src('test/**/*.js', {read: false})
@@ -9,6 +10,9 @@ gulp.task('servertests', function() {
 
 gulp.task('watch', function() {
   gulp.watch(['lib/**/*.js','test/**/*.js'], ['servertests']);
+});
+
+gulp.task('seed', function() {
 });
 
 gulp.task('default', ['watch']);
