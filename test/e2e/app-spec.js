8b7329e08a251e976747b2a6b9a9d4a60e780338
Nicholas Butterworth <nicholas.butterworth@gmail.com>
2015-07-15 13:06:15 -0400

    added app-spec.js in e2e test file

---
+++ test/e2e/app-spec.js
@@ -0,0 +1,14 @@
+/* taken directly from here: 
+http://thejackalofjavascript.com/end-to-end-testing-with-protractor/ */
+
+'use strict';
+/* https://github.com/angular/protractor/blob/master/docs/getting-started.md */
+describe('my app', function() {
+	beforeEach(function () {
+		browser.get('index.html');
+	});
+	
+	it('should automatically redirect to / when location hash is empty', function() {
+		expect(browser.getLocationAbsUrl()).toMatch("/");
+	}); 
+});
