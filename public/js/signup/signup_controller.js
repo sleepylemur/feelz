eff8b19725f55104362225be27e564a43910b60a
Colby Somerville <colby.somerville@gmail.com>
2015-07-15 13:10:27 -0400

    defining angular file structure

---
+++ public/js/signup/signup_controller.js
@@ -0,0 +1,13 @@
+angular.module('signup', [])
+  .controller('SignUpCtrl', function($scope, $http, $location, $window){
+    $scope.user = {}
+    $scope.signUp = function(){
+      $http.post('/signup', $scope.user)
+        .success(function(data, status, header, config){
+          $window.sessionStorage.token = data.token;
+          $location.path('/home');
+        }).error(function(data, status, header, config){
+          alert(status);
+        });
+      }
+    })
