9ff2f073d0c65e558e2f901d5995cab70553f9b9
Qiao Lin <qiaoclin@gmail.com>
2015-07-14 22:10:18 -0400

    auth

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,63 @@
-angular.module('vent', [])
+angular.module('vent', ['ngRoute'])
   .controller('testcontroller', function($scope) {
     $scope.message = 'hi!';
+  })
+  .controller('LoginCtrl', function($scope, $http){
+    $scope.user = {}
+    $scope.submit = function(){
+      $http.post('http://localhost:3000/login', $scope.user)
+      .success(function(data, status, header, config){
+        alert(data);
+      }).error(function(data, status, header, config){
+        alert(data);
+      });
+      
+    }
+  })
+  .controller('SignUpCtrl', function($scope, $http, $location, $window){
+    $scope.user = {}
+    $scope.signUp = function(){
+      $http.post('http://localhost:3000/signup', $scope.user)
+      .success(function(data, status, header, config){
+        $window.sessionStorage.token = data.token; 
+        $location.path('/home'); 
+      }).error(function(data, status, header, config){
+        alert(data);
+      });
+    }
+  })
+  .controller('homeCtrl', function($scope, $http){
+    $http.get('http://localhost:3000/api/secret').success(console.log);
+  })
+  .factory('authInterceptor', function($q, $window, $location) {
+   return {
+     request: function(config) {
+       config.headers = config.headers || {};
+       if ($window.sessionStorage.token) {
+         config.headers.Authorization = 'Bearer ' + $window.sessionStorage.token;
+       }
+       return config;
+     },
+     response: function(response) {
+       if (response.status === 401) {
+         delete $window.sessionStorage.token;
+         $location.path('login');
+       }
+       return response || $q.when(response);
+     }
+   };
+ })
+
+ .config(function($routeProvider, $httpProvider) {
+   $httpProvider.interceptors.push('authInterceptor');
+    $routeProvider
+      .when('/login', {
+        templateUrl: '../template/login.html'
+      })
+      .when('/signup', {
+        templateUrl: '../template/signup.html'
+      })
+      .when('/home', {
+        templateUrl: '../template/home.html'
+      })
   });
