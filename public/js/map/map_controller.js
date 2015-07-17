25f98c4e7ed0126e3cd2cd4958775fc0c510f964
Colby Somerville <colby.somerville@gmail.com>
2015-07-17 10:17:52 -0400

    new map controller iteration, bigger seed file

--- public/js/map/map_controller.js
+++ public/js/map/map_controller.js
@@ -6,53 +6,129 @@ angular.module('map', [])
       $scope.map.setCenter({lat: position.coords.latitude, lng: position.coords.longitude});
     });
 
+    // initializes map
     var options = {
       zoom: 14,
       styles: mapStyle
     }
+
     $scope.map = new google.maps.Map(document.getElementById('map-canvas'), options);
 
-    // initializes heat layer, GET request to the server.
-    $scope.heatLayer = function(){
-      $http.get('/api/emotions').success(function(data){
 
-        var locData = [];
-        data.forEach(function(e, i){
-          locData.push(new google.maps.LatLng(e.location[0], e.location[1]))
+    // getMapBounds used by requestPosts
+    $scope.getMapBounds = function(){
+      var bounds = $scope.map.getBounds();
+      var northEast = bounds.getNorthEast();
+      var southWest = bounds.getSouthWest();
+
+      return {n: northEast.lat(), e: northEast.lng(), s: southWest.lat(), w: southWest.lng()}
+    }
+
+    // GET request to server
+    $scope.requestPosts = function() {
+
+      var bounds = $scope.getMapBounds();
+      
+      $http.get('/api/emotions?n=' + bounds.n + '&e=' + bounds.e + '&s=' + bounds.s + '&w=' + bounds.w)
+        .then(function(data){
+          $scope.dataPoints = data.data;
+          disposeHeatLayers();
+          disposeMarkers();  
         })
+    }
 
-        $scope.heatmap = new google.maps.visualization.HeatmapLayer({data: locData});
-        $scope.heatmap.setMap($scope.map);
-      })
-    };
+    var disposeHeatLayers = function() {
 
+      var rants = [];
+      var raves = [];
 
+      // create google maps latlng objects split by emotion
+      angular.forEach($scope.dataPoints, function(e){
 
-    $scope.addMarkers = function(){
-      $http.get('listposts').success(function(data){
-        $scope.markers = data.map(function(e){
-          var marker = new google.maps.Marker({
-            position: new google.maps.LatLng(e.location[0], e.location[1]),
-            map: $scope.map,
-            title: e.message
-          })
-          google.maps.event.addListener(marker, 'click', function(){
-            console.log(e.message);
-          })
-          return marker;
-        });
+        if (e.location){
+
+          if (e.emotion === 'rant'){
+            rants.push(new google.maps.LatLng(e.location[0], e.location[1]));
+          } else {
+            raves.push(new google.maps.LatLng(e.location[0], e.location[1]));
+          }
+        }
+
       });
-    };
 
-    $scope.checkZoom = function(){
+      var gradient = [
+        'rgba(0, 255, 255, 0)',
+        'rgba(0, 255, 255, 1)',
+        'rgba(0, 191, 255, 1)',
+        'rgba(0, 127, 255, 1)',
+        'rgba(0, 63, 255, 1)',
+        'rgba(0, 0, 255, 1)',
+        'rgba(0, 0, 223, 1)',
+        'rgba(0, 0, 191, 1)',
+        'rgba(0, 0, 159, 1)',
+        'rgba(0, 0, 127, 1)',
+        'rgba(63, 0, 91, 1)',
+        'rgba(127, 0, 63, 1)',
+        'rgba(191, 0, 31, 1)',
+        'rgba(255, 0, 0, 1)'
+      ];
+
+      $scope.rantHeat = new google.maps.visualization.HeatmapLayer({data: rants});
+      $scope.raveHeat = new google.maps.visualization.HeatmapLayer({data: raves, gradient: gradient});
+
+      $scope.rantHeat.setMap($scope.map);
+      $scope.raveHeat.setMap($scope.map);
+    }
+
+    // checks to see whether markers should be added or removed
+    var disposeMarkers = function() {
       if ($scope.map.getZoom() > 15) {
-        $scope.addMarkers($scope.map)
-      } else if ($scope.markers){
-        $scope.removeMarkers($scope.map);
+        addMarkers();
+      } else if ($scope.markers && $scope.markers.length > 0) {
+        removeMarkers();
       }
-    };
-    google.maps.event.addListener($scope.map, 'zoom_changed', $scope.checkZoom)
+    }
 
-    $scope.heatLayer($scope.map);
+    var addMarkers = function() {
+
+      $scope.markers = [];
+
+      angular.forEach($scope.dataPoints, function(e){
+        var marker = new google.maps.Marker({
+          position: new google.maps.LatLng(e.location[0], e.location[1]),
+          map: $scope.map
+        });
+
+        // sets a click event on each individual marker
+        google.maps.event.addListener(marker, 'click', function(){
+          console.log(e.message)
+        });
+
+        // holds the markers on $scope so they can be removed later
+        $scope.markers.push(marker);
+      })
+    }
+
+    var removeMarkers = function() {
+      angular.forEach($scope.markers, function(e){
+        e.setMap(null);
+      });
+    }
+
+    $scope.addData = function(data){
+      // ensures initial trip to the server has been made
+      if ($scope.dataPoints) {
+        $scope.dataPoints.push(data);
+        disposeHeatLayers();
+        disposeMarkers();
+      }
+    }
+
+
+    // makes initial api call
+    google.maps.event.addListenerOnce($scope.map, 'tilesloaded', $scope.requestPosts);
+    
+    google.maps.event.addListener($scope.map, 'zoom_changed', disposeMarkers);
+
     return $scope;
   })
--- seed.js
+++ seed.js
@@ -1,14 +1,58 @@
 var db = require('./lib/db.js')();
 var models = require('./lib/models.js')(db);
+var faker = require('faker');
 
 // sync with force=true will drop tables and then rebuild the db from our models
 db.sync({force: true}).then(function() {
-  Promise.all([
-    models.User.create({email: 'e@e.com', password: 'e'}),
-    models.User.create({email: 'dave@dave.com', password: 'password'}),
-    models.Post.create({location: ['40.7127', '-74.0059'], message: 'ugh, i have no thoughts', emotion: 'duh', userId : 1}),
-    models.Post.create({location: ['40.7327', '-74.0259'], message: 'do refugees not have place to be?', emotion: 'agro', userId: 2})
-  ]).then(function(arr) {
+
+  var fakeUsers = [];
+
+  fakeUsers.push({username: 'e', email: 'e@e.com', password: 'e'})
+  fakeUsers.push({username: 'dave', email: 'dave@dave.com', password: 'password'})
+
+  // composes array of fake data objects
+  for (var i = 0; i < 100; i++){
+    fakeUsers.push({
+      username: faker.name.findName(),
+      email: faker.internet.email(),
+      password: faker.internet.password()
+    })
+  }
+
+  // feeds fake user data into the db
+  Promise.all(fakeUsers.map(function(e){
+    models.User.create({
+      username: e.username,
+      email: e.email,
+      password: e.password
+    })
+  })).then(function(){
+    // fake posts
+    fakePosts = [];
+
+    // randomly create the data
+    for (var i = 0; i < 500; i++){
+      fakePosts.push({
+        userId: Math.floor(Math.random() * 98 + 1),
+        location: [
+          Math.random() * 0.3 + 40.553,
+          Math.random() * 0.15 - 73.999 
+        ],
+        message: faker.lorem.sentence(),
+        emotion: (Math.floor(Math.random() * 2) === 0)? 'rant' : 'rave'
+      })
+    }
+
+    // and feed into the database
+    Promise.all(fakePosts.map(function(e){
+      models.Post.create({
+        userId: e.userId,
+        location: e.location,
+        message: e.message,
+        emotion: e.emotion
+      })
+    }))
+  }).then(function(arr) {
     // console.dir(arr[0].dataValues);
     console.log('db seeded');
     models.User.findAll().then(function(data) {
