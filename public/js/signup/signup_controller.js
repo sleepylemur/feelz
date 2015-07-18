5bad9a720f3e80325cb421f0e1b64902bb9982d1
Evan Griffiths <griffithse@gmail.com>
2015-07-17 22:20:40 -0400

    more complete signup validation ui

--- public/js/signup/signup_controller.js
+++ public/js/signup/signup_controller.js
@@ -2,23 +2,41 @@ angular.module('signup',[])
   .controller('SignUpCtrl', function($rootScope, $scope, $http, $location, $window){
     $scope.user = {};
     $scope.validateEmail = function() {
-      console.log($scope.user);
-      if (!$scope.user.email || !$scope.user.email.match(/^[^@\s]+@[^@\s]+\.[^@\s]+$/)) {
+      if (!$scope.user.email) {
+        $scope.emailerror = "email is required";
+      } else if (!$scope.user.email.match(/^[^@\s]+@[^@\s]+\.[^@\s]+$/)) {
         $scope.emailerror = "invalid email";
       }
     };
-    $scope.signUp = function(){
-      $http.post('/signup', $scope.user)
-        .success(function(data, status, header, config){
-          $window.sessionStorage.token = data.token;
-          /// initial client socket
-          $rootScope.currentuser = {id: data.id};
-          $rootScope.socket = io();
-          /// add user to socket with session token
-          $rootScope.socket.emit('addUser', $rootScope.currentuser);
-          $location.path('/map');
-        }).error(function(data, status, header, config){
-          alert(data.error);
-        });
+    $scope.validatePassword = function() {
+      if ($scope.user.password && $scope.user.password === $scope.user.confirmpassword) {
+        $scope.passworderror = "";
+      } else if ($scope.passwordblur && $scope.confirmblur) {
+        $scope.passworderror = "passwords don't match";
       }
-    })
+    };
+    $scope.signUp = function(){
+      if ($scope.passworderror || !$scope.user.password || $scope.user.password !== $scope.user.confirmpassword) {
+        $scope.passwordblur = true;
+        $scope.confirmblur = true;
+        $scope.validatePassword();
+        alert("passwords don't match");
+      } else if ($scope.emailerror || !$scope.user.email) {
+        $scope.validateEmail();
+        alert("invalid email");
+      } else {
+        $http.post('/signup', $scope.user)
+          .success(function(data, status, header, config){
+            $window.sessionStorage.token = data.token;
+            /// initial client socket
+            $rootScope.currentuser = {id: data.id};
+            $rootScope.socket = io();
+            /// add user to socket with session token
+            $rootScope.socket.emit('addUser', $rootScope.currentuser);
+            $location.path('/map');
+          }).error(function(data, status, header, config){
+            alert(data.error);
+          });
+      }
+    };
+  });
