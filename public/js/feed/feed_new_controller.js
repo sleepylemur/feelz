253cfb4f8539df4df03c0a38f30afd3097e74030
Evan Griffiths <griffithse@gmail.com>
2015-07-25 15:21:47 -0400

    error removal in feed view

--- public/js/feed/feed_new_controller.js
+++ public/js/feed/feed_new_controller.js
@@ -9,7 +9,6 @@ angular.module('feedNew', ['momentfilter','postService','voteService'])
     $(window).scroll(function(event) {
 
       var scrollTopVal = $(this).scrollTop();
-      console.log(scrollTopVal);
       if (scrollTopVal < 40) {
         $('header').css({top: '0px'});
         $('#feedToggleBar').css({top: '40px'});
