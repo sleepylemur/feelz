c572a5f6148cff0fa9ce02b1cc0cb2b79fe7ccab
Qiao Lin <qiaoclin@gmail.com>
2015-07-28 12:04:20 -0400

    profile page renders both currentUser & other users, toggle adminstration base on routeParams

--- public/js/profile/profile_controller.js
+++ public/js/profile/profile_controller.js
@@ -4,22 +4,11 @@ angular.module('profile', [])
 
     $scope.msg = 'heya';
     $scope.check = $routeParams.detail;
-  // check to see if an user id is passed 
-  if($routeParams.detail){
-    profileService.getOtherProfile(parseInt($routeParams.detail)).then(function(data) {
-      console.log('profile returns',data);
-      $scope.profile = data;
-    });
-  }else{
-    // if not then the profile route should render current user profile 
-    profileService.getProfile().then(function(data){ 
-      console.log("return profile data "+ data);
-      $scope.profile = data.user;
-      $scope.posts = data.posts; 
-    }); 
-  }
 
-
-
+  profileService.getProfile($routeParams.detail).then(function(data){ 
+    $scope.profile = data.data[0];
+    $scope.posts = data.data; 
+  }); 
   
+
 }])
